16882859ce64bcbd1fdde1728663492151a1bdfe
Thanh Doan <tmdoan98@gmail.com>
2020-05-24 17:50:27 +0100

    Simplify main function

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,79 +1,17 @@
+mod database;
+mod graphql;
+use database::Postgres;
+
 use anyhow::Result;
-use async_graphql::{http::playground_source, Context, EmptySubscription, FieldResult, Schema};
+use async_graphql::http::playground_source;
 use async_std::task;
-use chrono::{DateTime, Utc};
+use graphql::schema::{AppState, create_schema};
 use sqlx::PgPool;
 use std::env;
 use tide::{
     http::{headers, mime},
     Request, Response, StatusCode,
 };
-
-#[async_graphql::InputObject]
-struct NewIssue {
-    pub title: String,
-    pub description: String,
-}
-
-#[async_graphql::SimpleObject]
-struct Issue {
-    pub id: i32,
-    pub title: String,
-    pub description: String,
-    pub created_at: DateTime<Utc>,
-    pub updated_at: Option<DateTime<Utc>>,
-    pub category: Option<String>,
-}
-
-struct Query;
-
-#[async_graphql::Object]
-impl Query {
-    async fn issue(&self, ctx: &Context<'_>, id: i32) -> FieldResult<Option<Issue>> {
-        let pool: &PgPool = ctx.data();
-
-        let issue = sqlx::query_as!(
-            Issue,
-            "
-                SELECT *
-                FROM issue
-                WHERE id = $1
-            ",
-            id
-        )
-        .fetch_one(pool)
-        .await?;
-        Ok(Some(issue))
-    }
-}
-
-struct Mutation;
-
-#[async_graphql::Object]
-impl Mutation {
-    async fn create_issue(&self, ctx: &Context<'_>, new_issue: NewIssue) -> FieldResult<Issue> {
-        let pool: &PgPool = ctx.data();
-
-        let issue = sqlx::query_as!(
-            Issue,
-            "
-                INSERT INTO issue (title, description, created_at)
-                VALUES ($1, $2, timezone('UTC', now()))
-                RETURNING *
-            ",
-            &new_issue.title,
-            &new_issue.description
-        )
-        .fetch_one(pool)
-        .await?;
-
-        Ok(issue)
-    }
-}
-
-struct AppState {
-    schema: Schema<Query, Mutation, EmptySubscription>,
-}
 
 fn main() -> Result<()> {
     task::block_on(run())
@@ -87,10 +25,8 @@ async fn graphql(req: Request<AppState>) -> tide::Result<Response> {
 async fn run() -> Result<()> {
     let pool = PgPool::new("postgres://postgres:postgres@localhost:5432/postgres").await?;
 
-    let schema = Schema::build(Query, Mutation, EmptySubscription)
-        .data(pool)
-        .finish();
-
+    let database = Postgres::new(pool);
+    let schema = create_schema(database);
     let state = AppState { schema };
     let mut app = tide::with_state(state);
     app.at("/graphql").post(graphql).get(graphql);
